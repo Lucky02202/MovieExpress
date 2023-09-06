66a8f992d77b58909630d3d2ed49a5e34d4e0824
Laxmikant <laxmikant.patil02202@gmail.com>
2023-09-06 22:41:38 +0530

    Add default color pallets and fonts for the website design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl text-black font-semibold border-2 border-secondary px-4 py-3 bg-cta rounded-full w-fit m-4">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        palanquin: ["Palanquin", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#111827",
+        secondary: "#0F5A75",
+        border: "#00A9B2",
+        cta: "#61ECBC",
+      },
+    },
   },
   plugins: [],
 };
