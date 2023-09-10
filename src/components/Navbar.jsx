551dedb395bef6e72604ef868a0a80a8e0b516dd
Laxmikant <laxmikant.patil02202@gmail.com>
2023-09-11 00:24:10 +0530

    feat : add dropdown menu for mobile device's

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import img from '../assets/logo.svg'
+import { RiMenu3Line, RiCloseFill } from 'react-icons/ri'
 
-const Navbar = () => {
-
+const Options = () => {
   let [current, setCurrent] = useState([true, false, false, false, false])
 
   const handeClick = async (id) => {
@@ -20,18 +20,51 @@ const Navbar = () => {
   let [one, two, three, four, five] = current
 
   return (
-    <nav className="w-full box-border flex sm:flex-col ">
+
+    <ul className='flex flex-col sm:flex-row justify-between md:justify-around mt-4 md:mt-6 text-xs sm:text-sm md:text-base'>
+      <li className={`hover:li-hover px-4 py-3 rounded-xl ${one ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(0) }}>ALL MOVIES</li>
+      <li className={`hover:li-hover px-4 py-3 rounded-xl ${two ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(1) }}>TOP RATED</li>
+      <li className={`hover:li-hover px-4 py-3 rounded-xl ${three ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(2) }}>ACTION</li>
+      <li className={`hover:li-hover px-4 py-3 rounded-xl ${four ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(3) }}>DRAMA</li>
+      <li className={`hover:li-hover px-4 py-3 rounded-xl ${five ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(4) }}>ROMANCE</li>
+    </ul>
+
+  )
+}
+
+const Navbar = () => {
+
+  // useEffect(() => {
+  //   let menu = document.getElementById('menu')
+  //   menu.addEventListener("click", (e) => {
+  //     e.stopPropagation()
+  //   })
+  //   document.body.addEventListener("click", () => {
+  //     menu.style.display = "hidden"
+  //   })
+  // }, [])
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <nav className="w-full box-border flex sm:flex-col justify-evenly items-center">
       <div className="h-full w-full box-border text-white flex-1">
         <img src={img} alt="" className="w-full sm:w-3/5 md:w-2/5 m-auto" />
       </div>
-      <div className='flex-1'>
-        <ul className='flex justify-between md:justify-around mt-4 md:mt-6 text-xs sm:text-sm md:text-base'>
-          <li className={`hover:li-hover px-4 py-3 rounded-xl ${one ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(0) }}>ALL MOVIES</li>
-          <li className={`hover:li-hover px-4 py-3 rounded-xl ${two ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(1) }}>TOP RATED</li>
-          <li className={`hover:li-hover px-4 py-3 rounded-xl ${three ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(2) }}>ACTION</li>
-          <li className={`hover:li-hover px-4 py-3 rounded-xl ${four ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(3) }}>DRAMA</li>
-          <li className={`hover:li-hover px-4 py-3 rounded-xl ${five ? 'bg-gray-800 ' : null}`} onClick={() => { handeClick(4) }}>ROMANCE</li>
-        </ul>
+      <div className='hidden sm:block w-full'>
+        <Options />
+      </div>
+      <div className='flex sm:hidden flex-1 justify-end'>
+        {
+          toggleMenu
+            ? null
+            : <RiMenu3Line size={30} onClick={() => { setToggleMenu(!toggleMenu) }} />
+        }
+        {
+          toggleMenu &&
+          <div className='absolute top-0 right-0 animate-fade-left animate-once bg-slate-500 p-4 rounded-bl-xl h-screen' id='menu'>
+            <RiCloseFill size={20} className='absolute right-2 top-2' onClick={() => { setToggleMenu(!toggleMenu) }} />
+            <Options />
+          </div>
+        }
       </div>
     </nav>
   )
